97ca459a03ccc567f49a73227254741ea86fd46e
simon heglasik <simonheglasik@gmail.com>
2020-12-05 17:25:47 +0100

    spring web

---
+++ src/main/java/com/example/demo/appslab/EmployeeController.java
@@ -0,0 +1,45 @@
+package com.example.demo.appslab;
+
+import com.example.demo.appslab.EmployeeService;
+import com.example.demo.appslab.model.Driver;
+import com.example.demo.appslab.model.Programmer;
+import com.example.demo.appslab.model.Teacher;
+import com.example.demo.appslab.model.job;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+@RestController
+public class EmployeeController
+{
+    private EmployeeService ser;
+    List list = new ArrayList<job>();
+    public EmployeeController(EmployeeService ser)
+    {
+        this.ser = ser;
+        Collections.addAll(this.list,new Driver(500,200),new Teacher(333,222),new Programmer(333,444));
+    }
+    @GetMapping("/")
+    String hello()
+    {
+      return "Hello world";
+    }
+    @GetMapping("/salary")
+    double salary()
+    {
+        return ser.sumAllSalaries(list);
+    }
+    @GetMapping("/bonus")
+    float bonus()
+    {
+        return ser.sumAllBunuses(list);
+    }
+    @GetMapping("/snail")
+    float snail(@RequestParam double a, @RequestParam double b,@RequestParam double c)
+    {
+        return(float) Snail.snail(a,b,c);
+    }
+}
---
+++ src/main/java/com/example/demo/appslab/Snail.java
@@ -0,0 +1,10 @@
+package com.example.demo.appslab;
+
+
+public class Snail
+{
+    public static double snail(double heigh, double length, double towerheigh)
+    {
+        return ((towerheigh / heigh) * (length + heigh));
+    }
+}
